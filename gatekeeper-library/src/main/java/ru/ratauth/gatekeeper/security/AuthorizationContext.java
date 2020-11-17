5d2ec1c9970427c0c309b4438f548e75e1806db8
Aleksey Tokar <atokar2@alfabank.ru>
2020-11-17 10:13:19 +0300

    added serialVersionUID for normal update in hazelcast

--- gatekeeper-library/src/main/java/ru/ratauth/gatekeeper/security/AuthorizationContext.java
+++ gatekeeper-library/src/main/java/ru/ratauth/gatekeeper/security/AuthorizationContext.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class AuthorizationContext implements Serializable {
+
+    private static final long serialVersionUID = 8075229433221869854L;
+
     public static final String GATEKEEPER_AUTHORIZATION_CONTEXT_ATTR = "GATEKEEPER_AUTHORIZATION_CONTEXT";
 
     private final Map<String, ClientAuthorization> clientAuthorizations = new ConcurrentHashMap<>();
