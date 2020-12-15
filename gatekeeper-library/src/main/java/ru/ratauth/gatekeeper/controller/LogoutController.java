acf140940678a30fd5a4a6d07a698899dda3a886
tetiana.shykota <TShikota@alfabank.ru>
2020-12-15 18:04:42 +0200

    PASSPORT-4120 Changed logout URL endpoint

--- gatekeeper-library/src/main/java/ru/ratauth/gatekeeper/controller/LogoutController.java
+++ gatekeeper-library/src/main/java/ru/ratauth/gatekeeper/controller/LogoutController.java
@@ -2,7 +2,7 @@ package ru.ratauth.gatekeeper.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -14,8 +14,8 @@ public class LogoutController {
 
     private final LogoutService logoutService;
 
-    @GetMapping("/openid/logout/{client_id}")
-    public Mono<Void> callback(@PathVariable("client_id") String clientId, ServerWebExchange exchange) {
+    @GetMapping("/openid/logout")
+    public Mono<Void> callback(@RequestParam("client_id") String clientId, ServerWebExchange exchange) {
         return logoutService.performLogoutAndRedirect(clientId, exchange);
     }
 }
