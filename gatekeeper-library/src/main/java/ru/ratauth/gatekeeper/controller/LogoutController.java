6d435a95dc34b487863ffd37f7a953727e66d9de
AZamakhaev <l>
2021-01-22 13:06:33 +0300

    add jwt-token filter add context-url property

--- gatekeeper-library/src/main/java/ru/ratauth/gatekeeper/controller/LogoutController.java
+++ gatekeeper-library/src/main/java/ru/ratauth/gatekeeper/controller/LogoutController.java
@@ -2,6 +2,7 @@ package ru.ratauth.gatekeeper.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,11 +11,12 @@ import ru.ratauth.gatekeeper.service.LogoutService;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("${gatekeeper.context-url:/openid}")
 public class LogoutController {
 
     private final LogoutService logoutService;
 
-    @GetMapping("/openid/logout")
+    @GetMapping("/logout")
     public Mono<Void> callback(@RequestParam("client_id") String clientId, ServerWebExchange exchange) {
         return logoutService.performLogoutAndRedirect(clientId, exchange);
     }
